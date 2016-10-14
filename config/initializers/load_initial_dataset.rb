2aedce8890a9f03edd616b7a1f5ecd11794ba6bc
Jay Mendoza <jay.mendoza@bigcommerce.com>
2016-10-13 20:06:06 -0500

    move initial data load to initializer

---
+++ config/initializers/load_initial_dataset.rb
@@ -0,0 +1,11 @@
+url = 'https://www.bcstoretools.com/api/v1/call_data'
+auth = {:username => ENV['API_USER'],
+        :password => ENV['API_KEY']}
+
+HTTParty.get(url, :basic_auth => auth).each do |i|
+  Interval.create(api_id: i['id'],
+	          calls_offered: i['calls_offered'],
+	          calls_handled: i['calls_handled'],
+	          timestamp: i['timestamp'])
+end
+
