4b1edb91746698510b935063112eb1bc564a7028
Jay Mendoza <jay.mendoza@bigcommerce.com>
2016-10-13 23:00:14 -0500

    add basic highstock functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
+//= require highstock
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	root 'charts#main'
 	get '/intervals/all', to: 'intervals#all'
 	get '/intervals/between', to: 'intervals#between'
 end
