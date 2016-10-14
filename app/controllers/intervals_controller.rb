b3dc86b9a85a5d382879aa49c1ccd7ed9dd289d8
Jay Mendoza <jay.mendoza@bigcommerce.com>
2016-10-13 21:06:55 -0500

    add between API endpoint

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -1,6 +1,11 @@
 class IntervalsController < ApplicationController
-  def all
+	def all
 		@intervals = Interval.all
 		render json: @intervals
-  end
+	end
+
+	def between
+		@intervals = Interval.created_between(params[:min_date], params[:max_date])
+		render json: @intervals
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 	get '/intervals/all', to: 'intervals#all'
+	get '/intervals/between', to: 'intervals#between'
 end
