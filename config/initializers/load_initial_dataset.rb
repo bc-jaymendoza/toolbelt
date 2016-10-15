1b3c6a2751893cff3a31b63db1307fd136c6713b
Jay Mendoza <jay.mendoza@bigcommerce.com>
2016-10-15 14:03:36 -0500

    temp disable DB load job for deployment purposes

--- config/initializers/load_initial_dataset.rb
+++ config/initializers/load_initial_dataset.rb
@@ -1,11 +1,11 @@
-url = 'https://www.bcstoretools.com/api/v1/call_data?sort=asc'
-auth = {:username => ENV['API_USER'],
-        :password => ENV['API_KEY']}
+#url = 'https://www.bcstoretools.com/api/v1/call_data?sort=asc'
+#auth = {:username => ENV['API_USER'],
+#        :password => ENV['API_KEY']}
 
-HTTParty.get(url, :basic_auth => auth).each do |i|
-  Interval.create(api_id: i['id'],
-	          calls_offered: i['calls_offered'],
-	          calls_handled: i['calls_handled'],
-	          timestamp: i['timestamp'])
-end
+#HTTParty.get(url, :basic_auth => auth).each do |i|
+#  Interval.create(api_id: i['id'],
+#	          calls_offered: i['calls_offered'],
+#	          calls_handled: i['calls_handled'],
+#	          timestamp: i['timestamp'])
+#end
 
