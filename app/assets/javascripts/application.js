8a2b7a9e791452fd96a0900b075c4b9b859f5b63
Jay Mendoza <jay.mendoza@bigcommerce.com>
2016-10-17 08:48:56 -0500

    some front end stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require jquery
 //= require tether
 //= require bootstrap
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require highstock
 //= require jay
+//= require_tree .
--- vendor/assets/javascripts/jay.js
+++ vendor/assets/javascripts/jay.js
@@ -40,8 +40,8 @@ $(function () {
         });
     }
 
+    //initial data load
     $.each(names, function (i, name) {
-
         $.getJSON('/intervals/all?calls=' + name, function (data) {
 
             seriesOptions[i] = {
@@ -56,6 +56,51 @@ $(function () {
             }
         });
     });
-});
 
-// setInterval(function() { series.addPoint([startTime,startPoint], false, true); startPoint += 2; startTime += 900000; }, 1000);
+    //update data on a recurring basis
+    setInterval(function() {
+		var chart = $('#chart-container').highcharts();
+		series = chart.series[0];
+		recentGraphedPoints = series.points.slice(series.points.length-1, series.points.length);
+
+		//store 16 most recent intervals
+		var lastApiPoint = $.parseJSON($.ajax({
+			url:  '/intervals/last',
+			dataType: "json", 
+			async: false
+		}).responseText);
+		lastApiPoint.timestamp *= 1000;
+
+		lastPointGraphed = series.points.slice(-1).pop();
+
+		//updatedPointData.forEach(function(point) {
+		//	console.log(point.timestamp);
+		//});
+
+		console.log('last point: ' + lastPointGraphed.x);
+		console.log('updated point: ' + lastApiPoint.timestamp);
+
+		if (lastPointGraphed.x != lastApiPoint.timestamp) {
+			series.addPoint(lastApiPoint.timestamp, lastApiPoint.calls_offered)
+			chart.redraw();
+		}
+
+
+		//recentGraphedPoints.forEach(function(recentGraphedPoint) {
+		//	updatedPointData.forEach(function(updatedPoint) {
+				//update existing points
+		//		if (updatedPoint.timestamp === recentGraphedPoint.x) {
+				//	console.log('updatedPoint: ' + updatedPoint.timestamp)
+				//	console.log('recentGraphedPoint: ' + recentGraphedPoint.x)
+				//	recentGraphedPoint.update(updatedPoint.calls_offered);
+		//		} else {
+				// mis-match, i.e. we have a new point
+		//			series.addPoint(updatedPoint.timestamp, updatedPoint.calls_offered)
+		//			chart.redraw();
+		//			console.log('updatedPoint: ' + updatedPoint.timestamp)
+		//			console.log('recentGraphedPoint: ' + recentGraphedPoint.x)
+		//		}
+		//	});
+		//});
+	}, 2000);
+});
